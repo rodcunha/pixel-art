e61c73d7fff24336a5012efab295b6cfd84340ea
Rodrigo Cunha <rbpdvc@gmail.com>
2017-11-30 04:05:48 -0800

    Initial changes to the pixel art project

--- designs.js
+++ designs.js
@@ -1,10 +1,26 @@
 // Select color input
 // Select size input
+$(function() {  // Start jQuery DOM Ready function
 
+
+let $height = $('#input_height').val();   // variable declartion for the input Height
+let $width = $('#input_width').val();    // variable declaration for the input width
+const $submit = $('#submit');
+const $grid =  $('#pixel_canvas');
+
+$($submit).on(click, function() {
+  console.log($width * $height);
+});
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
+  let board;
+  board = height * width;
+  console.log(board);
 // Your code goes here!
 
 }
+makeGrid();
+
+
+}); // End of jQuery DOM Ready function

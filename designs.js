b3aedaa7ceb6e3930d0aed5ef945b51b2f16a0ae
Rodrigo Cunha <rbpdvc@gmail.com>
2017-12-01 08:55:03 -0800

    styling changes and some js functionality, still not geting valus from the input fields

--- designs.js
+++ designs.js
@@ -1,26 +1,30 @@
 // Select color input
 // Select size input
-$(function() {  // Start jQuery DOM Ready function
+  // Start jQuery DOM Ready function
 
 
-let $height = $('#input_height').val();   // variable declartion for the input Height
-let $width = $('#input_width').val();    // variable declaration for the input width
-const $submit = $('#submit');
-const $grid =  $('#pixel_canvas');
 
-$($submit).on(click, function() {
-  console.log($width * $height);
-});
 // When size is submitted by the user, call makeGrid()
+  const gridTable =  $('#pixel_canvas');
 
 function makeGrid() {
+  let height = $('#input_height').val();   // variable declartion for the input Height
+  let width = $('#input_width').val();    // variable declaration for the input width
   let board;
   board = height * width;
   console.log(board);
+
+  $('#submit').click(function() {
+    // console.log(width);
+    // console.log(height);
+  });
+
 // Your code goes here!
 
 }
 makeGrid();
 
+
+$(document).ready(function() {
 
 }); // End of jQuery DOM Ready function
